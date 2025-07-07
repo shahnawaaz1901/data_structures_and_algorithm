43ae3079b3c78d04ec59e6329c0a5eb1e7d74ac7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2025-07-07 17:58:56 +0530

    Add More Approaches

--- 04-Contains-duplicate.cpp
+++ 04-Contains-duplicate.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 // Problem : https://leetcode.com/problems/contains-duplicate/
+// Problem : https://www.geeksforgeeks.org/problems/check-if-array-contains-duplicates/1
+
+// Greedy
+class Solution
+{
+public:
+    bool checkDuplicates(vector<int> &arr)
+    {
+        // code here
+        for (int i = 0; i < arr.size(); i++)
+        {
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                if (arr[j] == arr[i])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// Better Approach
 class Solution
 {
 public:
@@ -19,6 +43,26 @@ public:
             {
                 return true;
             }
+        }
+        return false;
+    }
+};
+
+// Optimal Approach
+class Solution
+{
+public:
+    bool checkDuplicates(vector<int> &arr)
+    {
+        // code here
+        unordered_map<int, int> storedValues;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (storedValues[arr[i]])
+            {
+                return true;
+            }
+            storedValues[arr[i]]++;
         }
         return false;
     }
