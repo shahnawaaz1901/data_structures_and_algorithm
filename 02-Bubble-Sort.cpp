11bb02aa3e3603fd0e6ba57b8d584656143e5302
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2025-07-24 20:30:33 +0530

    Add Flag to Optimize Best Case

--- 02-Bubble-Sort.cpp
+++ 02-Bubble-Sort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Problem : https://www.geeksforgeeks.org/problems/bubble-sort/1
 
+// Bruteforce
 class Solution
 {
 public:
@@ -19,6 +20,34 @@ public:
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
                 }
+            }
+        }
+    }
+};
+
+// Optimize for Best Case
+class Solution
+{
+public:
+    void bubbleSort(vector<int> &arr)
+    {
+        // code here
+        for (int i = arr.size() - 1; i >= 0; i--)
+        {
+            bool areElementSwap = false;
+            for (int j = 1; j <= i; j++)
+            {
+                if (arr[j - 1] > arr[j])
+                {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    areElementSwap = true;
+                }
+            }
+            if (!areElementSwap)
+            {
+                break;
             }
         }
     }
